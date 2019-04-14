fd011e46a1fa972c3762d3c41c49bf7947c81471
Jaque <45607757+japerez107@users.noreply.github.com>
2019-04-14 03:49:36 -0400

    win/lose points

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,21 @@
 $(document).ready(function () {
 
+    // variables for win, lose and total
 
     var wins = 0;
-    var losses = 0;
+    var loss = 0;
     var totalScore = 0;
-
-    // $('#win').text(wins);
-
-    // $('#loss').text(losses);
-
     $('#total-score').text(totalScore);
 
+    // randomize gem values 
     var gem1 = Math.floor(Math.random() * 11 + 1);
     var gem2 = Math.floor(Math.random() * 11 + 1);
     var gem3 = Math.floor(Math.random() * 11 + 1);
     var gem4 = Math.floor(Math.random() * 11 + 1);
 
+    // randomize random number value
     var randomNum = Math.floor(Math.random() * 101) + 19;
-
     $("#randomNumber").text(randomNum);
-
 
     // click functions for images
 
@@ -27,72 +23,86 @@ $(document).ready(function () {
         totalScore = totalScore + gem1;
         console.log(totalScore);
         $("#total-score").text(totalScore);
+        winLose();
+
     });
 
     $("#gem2").on('click', function () {
         totalScore = totalScore + gem2;
         console.log(totalScore);
         $("#total-score").text(totalScore);
+        winLose();
+
     });
 
     $("#gem3").on('click', function () {
         totalScore = totalScore + gem3;
         console.log(totalScore);
         $("#total-score").text(totalScore);
+        winLose();
+
     });
 
     $("#gem4").on('click', function () {
         totalScore = totalScore + gem4;
         console.log(totalScore);
         $("#total-score").text(totalScore);
+        winLose();
     });
 
+    // function to decipher if the score wins or loses
+    var winLose = function () {
+        if (totalScore === randomNum) {
+
+            winner();
+
+            resetGem();
+        }
+        else if (randomNum < totalScore) {
+
+            loser();
+
+            resetGem();
+        }
+    };
 
 
-    if (randomNum > totalScore) {
-
-        losses++;
-        $('#loss').text(losses);
-
-        randomNum = Math.floor(Math.random() * 101) + 19;
-
-        totalScore = 0;
-
-    }
-
-    else if (totalScore === randomNum) {
+    // function to add a win point and reset random number and score
+    function winner() {
         wins++;
         $('#win').text(wins);
 
         randomNum = Math.floor(Math.random() * 101) + 19;
+        $("#randomNumber").text(randomNum);
 
         totalScore = 0;
-    }
+
+    };
+
+    // function to add a lose point and reset random number and score
+
+    function loser() {
+
+        loss++;
+        $('#loses').text(loss);
+
+        randomNum = Math.floor(Math.random() * 101) + 19;
+        $("#randomNumber").text(randomNum);
 
 
+        totalScore = 0;
+
+    };
 
 
-    // function winner() {
-    //     wins++;
+    // function to reset gem values 
+    function resetGem() {
+        gem1 = Math.floor(Math.random() * 11 + 1);
+        gem2 = Math.floor(Math.random() * 11 + 1);
+        gem3 = Math.floor(Math.random() * 11 + 1);
+        gem4 = Math.floor(Math.random() * 11 + 1);
 
-    //     $('#win').text(wins);
+    };
 
-
-    // }
-
-    // function loser() {
-    //     losses++;
-
-    //     $('#loss').text(losses);
-
-
-    // }
-
-
-    // function reset() {
-
-
-
-    // }
 });
 
